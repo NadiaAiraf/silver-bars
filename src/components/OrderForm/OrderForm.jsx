506667ede1aa62180436251314066915abd693a7
NadiaAiraf <aidanfaria@gmail.com>
2019-01-20 14:30:17 +0000

    modified Order Form to class to hold state for inputs

--- src/components/OrderForm/OrderForm.jsx
+++ src/components/OrderForm/OrderForm.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 
-const SellBoard = () => (
-    <div className="order-form-container">
-      <form className="order-form">
-        this is the order form<br />
-        <input type="text" value="" required placeHolder="user id" /><br />
-        <input type="text" value="" required placeHolder="order quantity"/><br />
-        <input type="text" value="" required placeHolder="price per kg"/><br />
-        <input type="text" value="" required placeHolder="buy or sell"/><br />
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
-)
+class OrderForm extends React.Component {
+  constructor(props) {
+    super(props);
+    
+  }
+  
+  render() {
+    return(
+      <div className="order-form-container">
+        <form className="order-form">
+          <h4>Order Input Form</h4>
+          <input type="text" value="" required placeHolder="user id" /><br />
+          <input type="text" value="" required placeHolder="order quantity"/><br />
+          <input type="text" value="" required placeHolder="price per kg"/><br />
+          <input type="radio" name="orderType" value="orderType" required />Buy<br />
+          <input type="radio" name="orderType" value="orderType" required />Sell<br />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    )
+  }
+}
 
-export default SellBoard;
+export default OrderForm;
